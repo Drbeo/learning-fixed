8f36ae702ddce4de4bb2e6d50fc0328dfdd4d5e0
Drbeo <adrbeo@gmail.com>
2020-07-01 18:33:04 +0800

    文件处理

--- JavaScript/Tools/Drbeo.js
+++ JavaScript/Tools/Drbeo.js
@@ -1,5 +1,89 @@
 const Drbeo = {
-    /***********************************************************************************
+    /**
+     * dataURL转Blob
+     * @param dataurl
+     * @return {Blob}
+     */
+    dataURLtoBlob(dataurl) {
+        let arr = dataurl.split(',');
+        let mime = arr[0].match(/:(.*?);/)[1];
+        let bstr = atob(arr[1]);
+        let n = bstr.length;
+        let u8arr = new Uint8Array(n);
+        while (n--) {
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new Blob([u8arr], {
+            type: mime
+        });
+    },
+    /**
+     * 图片base64转 Blob
+     * @param urlData {String} base64
+     * @param type {String} 图片mine
+     * @return {Blob} Blob
+     */
+    imageBase64ToBlob(urlData, type='image/jpeg') {
+        try {
+            let arr = urlData.split(',')
+            let mime = arr[0].match(/:(.*?);/)[1] || type;
+            // 去掉url的头，并转化为byte
+            let bytes = window.atob(arr[1]);
+            // 处理异常,将ascii码小于0的转换为大于0
+            let ab = new ArrayBuffer(bytes.length);
+            // 生成视图（直接针对内存）：8位无符号整数，长度1个字节
+            let ia = new Uint8Array(ab);
+
+            for (var i = 0; i < bytes.length; i++) {
+                ia[i] = bytes.charCodeAt(i);
+            }
+            return new Blob([ab], {
+                type: mime
+            });
+        } catch (e) {
+            let ab = new ArrayBuffer(0);
+            return new Blob([ab], {
+                type: type,
+            });
+        }
+    },
+     /**
+     * blob转本地url
+     */
+    blobToUrl(blob_data) {
+        return URL.createObjectURL(blob_data)
+    },
+    /**
+     * Blob 转base64
+     */
+    blobToBase64(blob_data, callback) {
+        let reader = new FileReader()
+        reader.onload = (e) => {
+            if (callback) {
+                callback(e.target.result)
+            }
+        }
+        reader.readAsDataURL(blob_data)
+    },
+        /**
+     * url转Blob
+     * @param the_url
+     * @param callback
+     */
+    urlToBlob(the_url, callback) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("get", the_url, true);
+        xhr.responseType = "blob";
+        xhr.onload = function () {
+            if (this.status == 200) {
+                if (callback) {
+                    callback(this.response);
+                }
+            }
+        };
+        xhr.send();
+    },
+        /***********************************************************************************
      *
      * 函数防抖（debounce）和函数节流（throttle）都是为了缓解函数频繁调用，它们相似，但有区别
      */
